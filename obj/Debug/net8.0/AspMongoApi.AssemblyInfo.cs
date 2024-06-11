c08650947928343323e8f6d46538af05217b5cfb
RaftaSV <gp0555032019@unab.edu.sv>
2024-06-11 12:13:26 -0600

    postMan file

--- obj/Debug/net8.0/AspMongoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/AspMongoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspMongoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+596cf04e770b04a5003a79dbf5484209f5013b9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dbea8aab711f8a32a27f9ecf04bfdb25a91d066")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspMongoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspMongoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
